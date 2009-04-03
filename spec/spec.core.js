f1279f07fee1f7d7c845d4a6dd26762d860a15d4
visionmedia <tj@vision-media.ca>
2009-04-03 10:58:28 -0700

    Refactored tons of specs to utilize nesting

--- spec/spec.core.js
+++ spec/spec.core.js
@@ -69,95 +69,157 @@ describe 'Positive specs'
 end
 
 describe 'Matchers'
+  describe 'eql'
+    it 'should work with strings'
+      'test'.should.eql 'test'
+      'test'.should.not.eql 'foo'
+    end
+    
+    it 'should work with numbers'
+      11.should.eql 11
+      10.should.not.eql 11
+    end
+    
+    it 'should loosely compare numbers as strings'
+      '11'.should.eql 11
+      '10'.should.not.eql 11
+    end
+    
+    it 'should hash compare arrays'
+      [1, 2].should.eql [1, 2]
+      [1, 2].should.not.eql [1, 3]
+      [1, 2, [3], { foo : 'bar' }].should.eql [1, 2, [3], { foo : 'bar' }]
+    end
+    
+    it 'should hash compare objects'
+      { foo : 'bar' }.should.eql { foo : 'bar' }
+    end
+  end
+  
+  describe 'equal'
+    it 'should perform strict comparisons'
+      'test'.should.equal 'test'
+      '1'.should.not.equal 1
+    end
+  end
+  
+  describe 'match'
+    it 'should match regular expressions'
+      'foobar'.should.match(/foo/)
+      'foobar'.should.not.match(/barfoo/)
+    end
+  end
+  
+  describe 'be_empty'
+    it 'should consider any object responding to a length of 0 to be empty'
+      ''.should.be_empty
+      ' '.should.not.be_empty
+      [].should.be_empty
+      { length : 0 }.should.be_empty
+    end
+  end
+  
+  describe 'be_null'
+    it 'should check if a value is null'
+      null.should.be_null
+      0.should.not.be_null
+    end
+  end
+  
+  describe 'have_length'
+    it 'should compare the length of an object'
+      'foo'.should.have_length 3
+      [1, 2].should.have_length 2
+    end
+  end
+  
+  describe 'have_length_within'
+    it 'should check if an object has a length within the specified range'
+      'foo'.should.have_length_within 2..4
+      'f'.should.not.have_length_within 2..4
+    end
+  end
+  
+  describe 'respond_to'
+    it 'should check if an object contains a method'
+      'test'.should.respond_to('toString')
+      'test'.should.not.respond_to('rawr')
+    end
+  end
+  
+  describe 'include'
+    it 'should check if an object includes a property'
+      { hey : 'there' }.should.include 'hey'
+      { hey : 'there' }.should.not.include 'foo'
+    end
+    
+    it 'should check if a regular expression includes a string'
+      /(foo)?bar/.should.include '(foo)'
+    end
+    
+    it 'should check if a function body includes a string'
+      -{ return [foo, bar] }.should.include 'foo', 'bar'
+      -{ return foo }.should.include(/foo|bar/)
+    end
+    
+    it 'should check if an array contains element(s)'
+      [1,2,3].should.include 1
+      [1,2,3].should.include 1, 2, 3
+      [1].should.not.include 0
+      ['foo', 'bar'].should.include 'foo', 'bar'
+      ['foo', 'bar'].should.include 'bar', 'foo'
+      ['foo', 'bar'].should.not.include 'foo', 'rawr'
+      ['foo', 'bar'].should.not.include 'rawr', 'foo'
+    end
+    
+    it 'should check hashes of array elements'
+      [1, [2]].should.include [2]
+      [1, [2]].should.include [2], 1
+      [1, { two : 'three' }].should.include { two : 'three' } 
+    end
+  end
+  
+  describe 'be_a'
+    it 'should compare the constructor of an object'
+      'test'.should.be_a String
+      [].should.be_an Array
+    end
+  end
 
-  it 'eql'
-    'test'.should_eql 'test'
-    '1'.should_eql 1
-    '1'.should_be 1
-    [1, 2].should_eql [1, 2]
-    [1, 2, [3], { foo : 'bar' }].should_eql [1, 2, [3], { foo : 'bar' }]
-    { foo : 'bar' }.should_eql { foo : 'bar' }
+  describe 'throw_error'
+    it 'should check if an error is thrown'
+      -{ throw 'error' }.should_throw_error
+      -{ return 'test' }.should_not_throw_error
+    end
+    
+    it 'should check if an error with a specific message is thrown'
+      -{ throw 'some foo bar' }.should.throw_error('some foo bar')
+      -{ throw 'some foo bar' }.should.throw_error(/foo bar/)
+      -{ throw 'some foo bar' }.should.not.throw_error(/rawr/)
+      -{ throw 'some foo bar' }.should.not.throw_error('rawr')
+    end
   end
   
-  it 'equal'
-    'test'.should_equal 'test' 
-    '1'.should_not_equal 1
+  describe 'be_type'
+    it 'should compare the type of an object via typeof'
+      'hey'.should.be_type 'string'
+      {}.should.be_type 'object'
+    end
+  end
+
+  describe 'be_within'
+    it 'should check if a number is within a range'
+      5.should.be_within 1..10
+      15.should.not.be_within 10..5
+    end
   end
   
-  it 'match'
-    'foobar'.should_match(/foo/)
-    'foobar'.should_not_match(/barfoo/)
-  end
-  
-  it 'be_empty'
-    var string = '', array = []
-    string.should_be_empty
-    array.should_be_empty
-  end
-  
-  it 'be_null'
-    var foo = null
-    foo.should_be_null
-    'test'.should_not_be_null
-  end
-  
-  it 'have_length'
-    'test'.should_have_length 4
-  end
-  
-  it 'have_length_within'
-    'foo'.should_have_length_within 2..4
-  end
-  
-  it 'respond_to'
-    'test'.should_not_respond_to('whatever')
-    'test'.should_respond_to('toString')
-  end
-  
-  it 'include'
-    123.should_include 1, 2, 3
-    { hey : 'there' }.should_include 'hey'
-    /(foo)?bar/.should_include '(foo)'
-    function(){ return foo }.should_include "return foo"
-    [1, 2, 3].should_include 2
-    [1, 2].should_include 1, 2
-    [1, [2]].should_include [2]
-    [1, 2, 3].should_not_include 5, 6
-    ['foo', 'bar'].should_include 'foo', 'bar'
-    ['foo', 'bar'].should_include 'bar', 'foo'
-    ['foo', 'bar'].should_not_include 'foo', 'rawr'
-    ['foo', 'bar'].should_not_include 'rawr', 'foo'
-  end
-  
-  it 'be_a'
-    'test'.should_be_a String
-    [].should_be_an Array
-  end
-  
-  it 'throw_error'
-    -{ throw 'error' }.should_throw_error
-    -{ return 'test' }.should_not_throw_error
-    -{ throw 'some foo bar' }.should.throw_error('some foo bar')
-    -{ throw 'some foo bar' }.should.throw_error(/foo bar/)
-    -{ throw 'some foo bar' }.should.not.throw_error(/rawr/)
-    -{ throw 'some foo bar' }.should.not.throw_error('rawr')
-  end
-  
-  it 'be_type'
-    'hey'.should_be_type('string')
-    {}.should_be_type('object')
-    function(){}.should_be_type('function')
-  end
-  
-  it 'be_within'
-    5.should_be_within 1..10
-    15.should_not_be_within 10..5
-  end
-  
-  it 'have'
-    person = { pets : ['izzy', 'niko'] }
-    person.should.have 2, 'pets'
-    person.should.not.have 3, 'pets'
+  describe 'have'
+    it 'should check the length of a property'
+      person = { pets : ['izzy', 'niko'] }
+      person.should.have 2, 'pets'
+      person.should.not.have 3, 'pets'
+    end
   end
   
   it 'have_at_least'
