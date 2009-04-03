4710ff8825193d29c91fad24636bd4fa956367f5
visionmedia <tj@vision-media.ca>
2009-04-03 15:21:33 -0700

    Added some more grammar specs

--- spec/spec.grammar.js
+++ spec/spec.grammar.js
@@ -10,6 +10,50 @@ describe 'JSpec grammar'
     n.should.eql 10
   end
   
+  it 'should allow parens to be optional when no args are passed'
+    true.should.be_true
+    true.should.be_true()
+  end
+  
+  it 'should allow parens to be optional with args'
+    'foobar'.should.include 'foo'
+    'rawr'.should.not_include 'foo'
+  end
+  
+  it 'should allow literals without defining variables variables'
+    {}.should.be_an Object
+  end
+  
+  it 'should allow alternative closure literal'
+    -{ throw 'test' }.should.throw_error
+  end
+  
+  it 'should allow . this. literal'
+    this.foo = 'bar'
+    .foo.should.eql 'bar'
+  end
+  
+  it 'should allow inclusive range literal n..n'
+    1..5.should.eql [1,2,3,4,5]
+    3..4.should.eql [3,4]
+    1..1.should.eql [1]
+    3..1.should.eql [3,2,1]
+  end
+  
+  it 'should allow snakecase style assertions'
+    'foo'.should_equal('foo')
+    'foo'.should_equal 'foo'
+    'bar'.should_not_equal('foo')
+    'bar'.should_not_equal 'foo'
+  end
+  
+  it 'should allow dot style assertions'
+    'foo'.should.equal('foo')
+    'foo'.should.equal 'foo'
+    'bar'.should.not.equal('foo')
+    'bar'.should.not.equal 'foo'
+  end
+  
   describe 'with nested describe'
     it 'should work'
       true.should.be_true
