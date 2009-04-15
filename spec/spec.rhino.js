83e50637392ffe01a4b519167be2c5e8a5196547
visionmedia <tj@vision-media.ca>
2009-04-15 14:13:25 -0700

    Added rhino spec

---
+++ spec/spec.rhino.js
@@ -0,0 +1,11 @@
+
+load('lib/jspec.js')
+
+// JSpec.options.formatter = JSpec.formatters.Terminal
+// JSpec
+// .exec('spec.grammar.js')
+// .exec('spec.core.js')
+// .exec('spec.core.dom.js')
+// .exec('spec.jquery.js')
+// .run()
+// .report()
