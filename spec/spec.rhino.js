0d1e7fb550d051f0cc5f29a29f50fe6ec2ec984b
visionmedia <tj@vision-media.ca>
2009-04-16 09:08:18 -0700

    Added support for options passed to run()

--- spec/spec.rhino.js
+++ spec/spec.rhino.js
@@ -1,11 +1,8 @@
 
 load('lib/jspec.js')
 
-JSpec.options.formatter = JSpec.formatters.Terminal
-JSpec.options.failuresOnly = true
-
 JSpec
 .exec('spec/spec.grammar.js')
 .exec('spec/spec.core.js')
-.run()
+.run({ formatter : JSpec.formatters.Terminal, failuresOnly : true })
 .report()
