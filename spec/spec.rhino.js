2e500f9b6225c45ed8a11e9197b044dc4211f651
visionmedia <tj@vision-media.ca>
2009-04-20 11:47:20 -0700

    Added grammar-less spec file

--- spec/spec.rhino.js
+++ spec/spec.rhino.js
@@ -3,6 +3,7 @@ load('lib/jspec.js')
 
 JSpec
 .exec('spec/spec.grammar.js')
+.exec('spec/spec.grammar-less.js')
 .exec('spec/spec.core.js')
 .run({ formatter : JSpec.formatters.Terminal, failuresOnly : true })
 .report()
