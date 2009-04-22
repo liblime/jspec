4f0bf5a2ea3ec23d25e912f46f3a607077276038
visionmedia <tj@vision-media.ca>
2009-04-22 16:35:54 -0700

    Added JSpec.contentsOf()

--- spec/spec.grammar-less.js
+++ spec/spec.grammar-less.js
@@ -12,21 +12,25 @@ JSpec.describe('Grammar-less', function(){
   })
   
   describe('with nested describes', function(){
+    var hits = []
+    
     before_each(function(){
-      n++  
+      n++
+      hits.push('before_each')
     })
     
     it('should work', function(){
       expect(true).not_to('be', false)
       expect(n).to('eql', 2)
+      expect(hits).to('eql', ['before_each'])
     })
     
     foo = 'bar'
-    
     it('should work again', function(){
       expect(n).to('eql', 3)
       expect(foo).to('eql', 'bar')
       expect('foo').to('eql', 'bar')
+      expect(hits).to('eql', ['before_each', 'before_each'])
     })
   })
   
