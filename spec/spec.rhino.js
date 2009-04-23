00b11d549f11dc2d90022c038728f6909704fbff
visionmedia <tj@vision-media.ca>
2009-04-23 12:07:02 -0700

    Added spec.matchers.js

--- spec/spec.rhino.js
+++ spec/spec.rhino.js
@@ -5,6 +5,7 @@ load('spec/spec.grammar-less.js')
 JSpec
 .exec('spec/spec.grammar.js')
 .exec('spec/spec.js')
+.exec('spec/spec.matchers.js')
 .exec('spec/spec.utils.js')
 .run({ formatter : JSpec.formatters.Terminal, failuresOnly : true })
 .report()
