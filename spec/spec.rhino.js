7a10fa6453406a0bb5c3388fc69d19d3c30bebea
visionmedia <tj@vision-media.ca>
2009-04-23 08:34:13 -0700

    Proxy assertions executing properly

--- spec/spec.rhino.js
+++ spec/spec.rhino.js
@@ -5,5 +5,5 @@ load('spec/spec.grammar-less.js')
 JSpec
 .exec('spec/spec.grammar.js')
 .exec('spec/spec.core.js')
-.run({ formatter : JSpec.formatters.Terminal, failuresOnly : false })
+.run({ formatter : JSpec.formatters.Terminal, failuresOnly : true })
 .report()
