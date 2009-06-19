309c3c0c0a780efc38db314578db853e0e19b5ec
visionmedia <tj@vision-media.ca>
2009-06-19 14:02:32 -0700

    Fixed throw_error error messages for Opera

--- spec/spec.matchers.js
+++ spec/spec.matchers.js
@@ -196,7 +196,8 @@ describe 'Matchers'
     it 'should check if an error of a specific constructor is thrown'
       -{ throw new Error('foo') }.should.throw_error(Error)
       -{ throw new TypeError('foo') }.should.throw_error(TypeError)
-      -{ throw 'foo' }.should.not.throw_error(Error)
+      -{ throw 'foo' }.should.throw_error Error
+      -{ throw 'foo' }.should.not.throw_error TypeError
     end
     
     it 'should check if an error with a specific constructor and message is thrown'
