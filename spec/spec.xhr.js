5f69a2c13495bef91119cd90ac9459aa90f7813b
visionmedia <tj@vision-media.ca>
2009-07-03 16:16:41 -0700

    mockRequest property

--- spec/spec.xhr.js
+++ spec/spec.xhr.js
@@ -1,15 +1,15 @@
 
 describe 'JSpec'
-  describe '.mockRequest()'
+  describe '.mockRequest'
     it 'should mock XMLHttpRequests un unmockRequest() is called or the spec block has finished'
-      mockRequest()
+      mockRequest.and_return('test')
       XMLHttpRequest.should.eql JSpec.XMLHttpRequest
       unmockRequest()
       XMLHttpRequest.should.not.eql JSpec.XMLHttpRequest
     end
     
-    it 'should mock specific request URIs'
-      mockRequest('test').and_return('{ foo : "bar" }', 'application/json', 200)
+    it 'should mock XMLHttpRequests'
+      mockRequest.and_return('{ foo : "bar" }', 'application/json', 200)
       $.getJSON('test', function(response){
         response.should.eql { foo : 'bar' }
       })
@@ -20,15 +20,8 @@ describe 'JSpec'
       XMLHttpRequest.should.not.eql JSpec.XMLHttpRequest
     end
     
-    it 'should mock request URIs matching a regexp'
-      mockRequest(/^foo(bar)?/).and_return('{ foo : "bar" }', 'application/json', 200)
-      $.getJSON('foobar', function(response){
-        response.should.eql { foo : 'bar' }
-      })
-    end
-    
     it 'should populate textStatus'
-      mockRequest('foo').and_return('bar', 'text/plain', 404)
+      mockRequest.and_return('bar', 'text/plain', 404)
       $.getJSON('foo', function(response, textStatus){
         textStatus.should.eql 'Not Found'
       })
