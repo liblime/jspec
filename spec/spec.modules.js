1cdc2301e50a0d955e8979d167557b3e36d01f97
visionmedia <tj@vision-media.ca>
2009-07-03 09:27:35 -0700

    Moving module specs

--- spec/spec.modules.js
+++ spec/spec.modules.js
@@ -1,23 +1,25 @@
 
 describe 'JSpec'
-  describe 'modules'
-    it 'should run in context with beforeSpec'
-      addedBeforeSpec.should.be_true
-      addedAfterSpec.should.not.be_true
-    end
-    
-    it 'should run in context with afterSpec'
-      addedAfterSpec.should.be_true
-    end
-    
-    it 'should run in context with beforeSuite'
-      addedBeforeSuite.should.be_true
-      addedAfterSuite.should.not.be_true
-    end
-    
-    describe 'another suite'
-      it 'should run in context with afterSuite'
-        addedAfterSuite.should.be_true
+  describe 'module'
+    describe 'hooks'
+      it 'should run in context with beforeSpec'
+        addedBeforeSpec.should.be_true
+        addedAfterSpec.should.be_false
+      end
+
+      it 'should run in context with afterSpec'
+        addedAfterSpec.should.be_true
+      end
+
+      it 'should run in context with beforeSuite'
+        addedBeforeSuite.should.be_true
+        addedAfterSuite.should.be_false
+      end
+
+      describe 'another suite'
+        it 'should run in context with afterSuite'
+          addedAfterSuite.should.be_true
+        end
       end
     end
     
