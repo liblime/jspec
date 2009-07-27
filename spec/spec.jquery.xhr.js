987486a36aa088b3f44fdb4d33443067a1c2bf5d
visionmedia <tj@vision-media.ca>
2009-07-27 10:29:21 -0700

    More specs

--- spec/spec.jquery.xhr.js
+++ spec/spec.jquery.xhr.js
@@ -8,5 +8,21 @@ describe 'jQuery'
         statusText.should.eql 'success'
       })
     end
+    
+    it 'should not invoke callback when response status is 4xx'
+      mockRequest.and_return('foo', 'text/plain', 404)
+      $.getJSON('foo', function(){
+        fail('callback was invoked')
+      })
+    end
+  end
+  
+  describe '.post()'
+    it 'should work with mockRequest'
+      mockRequest.and_return('<p></p>', 'text/html')
+      $.post('foo', function(response){
+        response.should.eql '<p></p>'
+      })
+    end
   end
 end
