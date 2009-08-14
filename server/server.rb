2eea46ebe5eac0887a87c8d463a9d91062e16912
visionmedia <tj@vision-media.ca>
2009-08-14 16:40:08 -0700

    Docs for the ruby server

--- server/server.rb
+++ server/server.rb
@@ -5,13 +5,33 @@ require 'server/browsers'
 
 module JSpec
   class Server
-    attr_reader :responses, :browsers, :root
+    
+    ##
+    # Array of responses.
+    
+    attr_reader :responses
+    
+    ##
+    # Array of browser names to launch.
+    
+    attr_reader :browsers
+    
+    ##
+    # Server root.
+    
+    attr_reader :root
+    
+    ##
+    # Initialize with _options_.
     
     def initialize options = {}
       @responses = []
       @browsers = options.delete :browsers
       @root = options.delete :root
     end
+    
+    ##
+    # Handle rack requests.
     
     def call env
       request = Rack::Request.new env
@@ -32,10 +52,17 @@ module JSpec
       end
       [200, { 'Content-Type' => type, 'Content-Length' => body.length.to_s }, body]
     end
+    
+    ##
+    # Output results with the given _browser_, number
+    # of _failures_ and _passes_.
 
     def display_results browser, failures, passes
       puts '%-14s - passes: %s failures: %s' % [bold(browser), green(passes), red(failures)]
     end
+    
+    ##
+    # Return brower name symbol from user agent _string_.
 
     def browser string
       case string
@@ -46,22 +73,37 @@ module JSpec
       when /Opera/   ; :Opera
       end
     end
+    
+    ##
+    # Bold _string_.
 
     def bold string
       color string, 1
     end
+    
+    ##
+    # Red _string_.
 
     def red string
       color string, 31
     end
+    
+    ##
+    # Green _string_.
 
     def green string
       color string, 32
     end
+    
+    ##
+    # Color _string_ with ansi escape _code_.
 
     def color string, code
       "\e[#{code}m#{string}\e[m"
     end
+    
+    ##
+    # Call _block_ when all servers have responded.
     
     def when_finished &block
       Thread.new {
@@ -69,6 +111,9 @@ module JSpec
         yield
       }
     end
+    
+    ##
+    # Start new JSpec server with _options_.
     
     def self.start options, spec
       app = Rack::Builder.new do
@@ -88,14 +133,21 @@ module JSpec
       self
     end
     
+    ##
+    # Run _browsers_.
+    
     def self.run_browsers browsers, spec
       browsers.each do |name|
         browser(name).open "http://localhost:4444/#{spec}"
       end
     end
     
+    ##
+    # Return browser class for _name_.
+    
     def self.browser name
-      eval("JSpec::Browser::#{name}").new
+      JSpec::Browser.const_get(name.to_sym).new
     end
+    
   end
 end
