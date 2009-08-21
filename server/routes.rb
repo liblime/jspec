d297b584ddcdf75fbe45365841857acde2596b10
visionmedia <tj@vision-media.ca>
2009-08-21 10:32:56 -0700

    Server output matching Rhino

--- server/routes.rb
+++ server/routes.rb
@@ -7,13 +7,20 @@ post '/results' do
   require 'json'
   data = JSON.parse request.body.read
   if data['options'].include?('verbose') && data['options']['verbose']
-    puts "\n\n%20s Passes: %s Failures: %s\n\n" % [bold(browser_name), green(data['stats']['passes']), red(data['stats']['failures'])]
+    puts "\n\n %s Passes: %s Failures: %s\n\n" % [bold(browser_name), green(data['stats']['passes']), red(data['stats']['failures'])]
     data['results'].compact.each do |suite|
-      puts "\n  " + bold(suite['description'])
+      puts "\n " + bold(suite['description'])
       suite['specs'].compact.each do |spec|
-        next if spec['status'] == 'pass' && data['options'].include?('failuresOnly') && data['options']['failuresOnly'] 
-        puts '    ' +  send(spec['status'] == 'pending' ? :blue : :green, spec['description']) + (assertion_graph_for(spec['assertions']) || '')
-        puts '      ' +  red(spec['message']) if spec['message']
+        case spec['status'].to_sym
+        when :pass 
+          next if data['options'].include?('failuresOnly') && data['options']['failuresOnly']
+          puts '  ' + green(spec['description']) + assertion_graph_for(spec['assertions']).to_s
+        when :fail
+          puts '  ' + red(spec['description'])
+          puts '  ' + spec['message'] + "\n\n" if spec['message']
+        else
+          puts '  ' + blue(spec['description'])
+        end
       end
     end
   else
