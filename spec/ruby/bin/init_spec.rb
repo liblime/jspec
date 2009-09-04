90a9d60f759de8886f35eb1d73b0a08b1c86f595
visionmedia <tj@vision-media.ca>
2009-09-04 13:40:26 -0700

    Started crappy ruby specs

--- spec/ruby/bin/init_spec.rb
+++ spec/ruby/bin/init_spec.rb
@@ -1,9 +1,22 @@
 
+include FileUtils
 
 describe "jspec" do
   describe "init" do
     it "should initialize a default project at the given path" do
-      jspec('init', File.dirname(__FILE__) + '/test')
+      dest = File.dirname(__FILE__) + '/test'
+      jspec('init', dest)
+      File.directory?(dest).should be_true
+      rm_rf dest
+    end
+    
+    it "should initialize a rails project when using -R or --rails" do
+      dest = File.dirname(__FILE__) + '/test'
+      mkdir dest
+      mkdir dest + '/vendor'
+      jspec('init', dest, '--rails')
+      File.directory?(dest).should be_true
+      rm_rf dest
     end
   end
 end
--- spec/ruby/bin/spec_helper.rb
+++ spec/ruby/bin/spec_helper.rb
@@ -1,6 +1,8 @@
 
+require 'fileutils'
+
 BIN = File.dirname(__FILE__) + '/../../../bin/jspec'
 
 def jspec *args
-  system BIN, args
+  system BIN, *args
 end
