b2ed7e8f97570261f223eff40913f83c9c490fd4
visionmedia <tj@vision-media.ca>
2009-09-04 13:33:12 -0700

    Setting up more specs

--- spec/ruby/bin/init_spec.rb
+++ spec/ruby/bin/init_spec.rb
@@ -1,6 +1,9 @@
 
-describe "foo" do
-  it "should description" do
-    
+
+describe "jspec" do
+  describe "init" do
+    it "should initialize a default project at the given path" do
+      jspec('init', File.dirname(__FILE__) + '/test')
+    end
   end
 end
---
+++ spec/ruby/bin/spec_helper.rb
@@ -0,0 +1,6 @@
+
+BIN = File.dirname(__FILE__) + '/../../../bin/jspec'
+
+def jspec *args
+  system BIN, args
+end
