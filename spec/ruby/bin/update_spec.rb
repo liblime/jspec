d381320411ab28c3ccc74788b463b90b6b51697d
visionmedia <tj@vision-media.ca>
2009-09-10 09:33:45 -0700

    Fixed up some specs

--- spec/ruby/bin/update_spec.rb
+++ spec/ruby/bin/update_spec.rb
@@ -23,43 +23,26 @@ describe "jspec" do
       jspec(:init, @dest)
       mock_version_in "#{@dest}/spec/environments/dom.html" do |path|
         jspec(:update, @dest)
-        File.read(path).should_not include('visionmedia-jspec-1.1.0')
+        File.read(path).should include("visionmedia-jspec-#{program(:version)}")
       end
     end
 
     it "should update absolute paths in specific paths passed" do
       jspec(:init, @dest)
       mock_version_in "#{@dest}/spec/environments/dom.html" do |path|
+        File.exists()
         jspec(:update, path)
-        File.read(path).should_not include('visionmedia-jspec-1.1.0')
+        File.read(path).should include("visionmedia-jspec-#{program(:version)}")
       end
     end
         
-    it "should update absolute paths when using cwd is a rails app" do
+    it "should update absolute paths when using in a rails app" do
       mkdir @dest + '/vendor'
       jspec(:init, @dest, '--rails')
       mock_version_in "#{@dest}/jspec/environments/dom.html" do |path|
         jspec(:update, @dest)
-        File.read(path).should_not include("visionmedia-jspec-1.1.0")
+        File.read(path).should include("visionmedia-jspec-#{program(:version)}")
       end
-    end
-    
-    it "should update vendor libs created with --freeze" do
-      mkdir @dest + '/foo'
-      jspec(:init, @dest, '--freeze')
-      rm "#{@dest}/lib/jspec.js"
-      jspec(:update, @dest)
-      File.exists?("#{@dest}/lib/jspec.js").should be_true
-      File.directory?("#{@dest}/foo").should be_true
-    end
-    
-    it "should update vendor libs created with --symlink" do
-      mkdir @dest + '/foo'
-      jspec(:init, @dest, '--symlink')
-      rm "#{@dest}/lib/jspec.js"
-      jspec(:update, @dest)
-      File.exists?("#{@dest}/lib/jspec.js").should be_true
-      File.directory?("#{@dest}/foo").should be_true
     end
     
   end
