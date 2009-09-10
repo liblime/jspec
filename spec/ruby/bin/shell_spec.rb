d00befc771ec6994bc7b010bfd9467d539f4ac21
visionmedia <tj@vision-media.ca>
2009-09-10 11:03:19 -0700

    Specs for shell quit / exit commands

--- spec/ruby/bin/shell_spec.rb
+++ spec/ruby/bin/shell_spec.rb
@@ -4,5 +4,10 @@ describe "jspec" do
     it "should load jspec.js" do
       shell('print(JSpec.version)', 'quit()').should include('JSpec')
     end
+    
+    it "should quit when quit or exit commands are called" do
+      shell('print(JSpec.version)', 'quit').should include('JSpec')
+      shell('print(JSpec.version)', 'exit').should include('JSpec')
+    end
   end
 end
