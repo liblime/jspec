889026dbd969bcde6aeed31a2d46a46f55e186be
visionmedia <tj@vision-media.ca>
2009-09-10 10:06:59 -0700

    More update specs

--- spec/ruby/bin/update_spec.rb
+++ spec/ruby/bin/update_spec.rb
@@ -22,12 +22,29 @@ describe "jspec" do
     it "should update absolute paths matching visionmedia-jspec-n.n.n" do
       jspec(:init, @dest)
       mock_version_in "#{@dest}/spec/environments/dom.html" do |path|
-        jspec(:update, @dest)
+        Dir.chdir(@dest) { jspec(:update) }
         File.read(path).should include("visionmedia-jspec-#{program(:version)}")
       end
     end
     
     it "should update absolute paths matching visionmedia-jspec-n.n.n for a rails project" do
+      mkdir @dest + '/vendor'
+      jspec(:init, @dest)
+      mock_version_in "#{@dest}/jspec/environments/dom.html" do |path|
+        Dir.chdir(@dest) { jspec(:update) }
+        File.read(path).should include("visionmedia-jspec-#{program(:version)}")
+      end
+    end
+    
+    it "should update absolute paths matching visionmedia-jspec-n.n.n when a destination is passed" do
+      jspec(:init, @dest)
+      mock_version_in "#{@dest}/spec/environments/dom.html" do |path|
+        jspec(:update, @dest)
+        File.read(path).should include("visionmedia-jspec-#{program(:version)}")
+      end
+    end
+    
+    it "should update absolute paths matching visionmedia-jspec-n.n.n for a rails project when a destination in passed" do
       mkdir @dest + '/vendor'
       jspec(:init, @dest)
       mock_version_in "#{@dest}/jspec/environments/dom.html" do |path|
