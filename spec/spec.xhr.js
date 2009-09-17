08f88744a648e26706ae0f58a84aa0f6cc7acfa1
visionmedia <tj@vision-media.ca>
2009-09-17 10:03:15 -0700

    Fixed fixture()
    Now relative to spec/environments

--- spec/spec.xhr.js
+++ spec/spec.xhr.js
@@ -99,7 +99,7 @@ describe 'JSpec'
       
       it 'should not interrupt JSpec request related functionality'
         mockRequest().and_return('fail')
-        (JSpec.tryLoading('async') || JSpec.tryLoading('spec/async')).should.eql 'cookies!'
+        (JSpec.tryLoading('../async') || JSpec.tryLoading('async')).should.eql 'cookies!'
         fixture('test').should.eql '<p>test</p>'
         fixture('test.json').should.include '{ user'
       end
