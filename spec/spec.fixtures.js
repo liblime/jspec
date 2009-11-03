897d8fb0727ecd3d87faa7def9af2c999768fa3b
visionmedia <tj@vision-media.ca>
2009-11-03 15:54:04 -0800

    Added fixturePath option

--- spec/spec.fixtures.js
+++ spec/spec.fixtures.js
@@ -2,7 +2,6 @@
 describe 'Utility'
   describe 'fixture()'
     it 'should return a files contents'
-      fixture('fixtures/test.html').should.eql '<p>test</p>'
       fixture('test.html').should.eql '<p>test</p>'
       fixture('test').should.eql '<p>test</p>'
     end
