89235df6e79c603af1348e61a8d0b8b27663cdcb
visionmedia <tj@vision-media.ca>
2009-12-03 16:24:17 -0800

    Added specs for node.js
    Fixtures are currently bunk

--- spec/environments/node.js
+++ spec/environments/node.js
@@ -1,46 +1,32 @@
 
-__loading__ = []
-__loadDelay__ = 1000
+require.paths.unshift('./lib', './spec');
+process.mixin(require('sys'))
 
-originalPrint = print
+require("jspec")
+require("helpers")
+
+quit = process.exit
 print = puts
 
-readFile = function(path, callback) {
-  __loading__.push(path)
-  var promise = node.fs.cat(path, "utf8")
+readFile = function(path) {
+  var promise = require('posix').cat(path, "utf8")
+  var result = ''
   promise.addErrback(function(){ throw "failed to read file `" + path + "'" })
   promise.addCallback(function(contents){
-    setTimeout(function(){
-      if (__loading__[0] == path)
-        __loading__.shift(), callback(contents)
-      else
-        setTimeout(arguments.callee, 50)
-    }, 50)
-  })  
-}
-
-load = function(path) {
-  readFile(path, function(contents){
-    eval(contents)
+    result = contents
   })
+  promise.wait()
+  return result
 }
 
-load('lib/jspec.js')
-load('spec/modules.js')
-load('spec/spec.grammar-less.js')
-
-setTimeout(function(){
-  JSpec
-  .exec('spec/spec.grammar.js')
-  .exec('spec/spec.js')
-  .exec('spec/spec.matchers.js')
-  .exec('spec/spec.utils.js')
-  .exec('spec/spec.shared-behaviors.js')
-  setTimeout(function(){ 
-    JSpec.run({ reporter: JSpec.reporters.Terminal, failuresOnly: false })
-    setTimeout(function() {
-      JSpec.report()
-    }, __loadDelay__ / 3)
-  }, __loadDelay__ / 3)
-}, __loadDelay__ / 3)
-
+JSpec
+.exec('spec/spec.js')
+.exec('spec/spec.utils.js')
+.exec('spec/spec.modules.js')
+.exec('spec/spec.matchers.js')
+.exec('spec/spec.shared-behaviors.js')
+.exec('spec/spec.grammar.js')
+.exec('spec/spec.grammar-less.js')
+.exec('spec/spec.fixtures.js')
+.run({ reporter: JSpec.reporters.Terminal, failuresOnly: true })
+.report()
