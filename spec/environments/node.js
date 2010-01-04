fff794ac26be20358404280669f01c036f3d6dc4
visionmedia <tj@vision-media.ca>
2010-01-04 12:41:13 -0800

    Fixed node.js runner, and moved specs to spec/unit

--- spec/environments/node.js
+++ spec/environments/node.js
@@ -11,7 +11,7 @@ print = puts
 readFile = function(path) {
   var promise = require('posix').cat(path, "utf8")
   var result = ''
-  promise.addErrback(function(){ throw "failed to read file `" + path + "'" })
+  promise.addErrback(function(){ throw new Error("failed to read file `" + path + "'") })
   promise.addCallback(function(contents){
     result = contents
   })
@@ -20,13 +20,13 @@ readFile = function(path) {
 }
 
 JSpec
-.exec('spec/spec.js')
-.exec('spec/spec.utils.js')
-.exec('spec/spec.modules.js')
-.exec('spec/spec.matchers.js')
-.exec('spec/spec.shared-behaviors.js')
-.exec('spec/spec.grammar.js')
-.exec('spec/spec.grammar-less.js')
-.exec('spec/spec.fixtures.js')
-.run({ reporter: JSpec.reporters.Terminal, failuresOnly: true })
+.exec('spec/unit/spec.js')
+.exec('spec/unit/spec.utils.js')
+.exec('spec/unit/spec.modules.js')
+.exec('spec/unit/spec.matchers.js')
+.exec('spec/unit/spec.shared-behaviors.js')
+.exec('spec/unit/spec.grammar.js')
+.exec('spec/unit/spec.grammar-less.js')
+//.exec('spec/unit/spec.fixtures.js') TODO: when exceptions bubble properly uncomment
+.run({ reporter: JSpec.reporters.Terminal, failuresOnly: true, fixturePath: 'spec/fixtures' })
 .report()
