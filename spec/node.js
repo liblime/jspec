7bad07caec7253993ff5334ad2f0a8a6a5c7ac03
visionmedia <tj@vision-media.ca>
2010-02-03 07:51:20 -0800

    Moved node.js support from template to core
    this means you can now use a spec runner file as if
    it was a reqular CommonJS module, no node.js specific
    code

--- spec/node.js
+++ spec/node.js
@@ -1,22 +1,8 @@
 
 require.paths.unshift('./lib', './spec');
-process.mixin(require('sys'))
 
 require("jspec")
 require("unit/helpers")
-
-quit = process.exit
-print = puts
-
-readFile = function(path) {
-  var result
-  require('posix')
-    .cat(path, "utf8")
-    .addCallback(function(contents){ result = contents })
-    .addErrback(function(){ throw new Error("failed to read file `" + path + "'") })
-    .wait()
-  return result
-}
 
 JSpec
 .exec('spec/unit/spec.js')
