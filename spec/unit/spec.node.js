7a3933c49a9d2d77a38a5d396ebfc5fd8938f5a0
visionmedia <tj@vision-media.ca>
2010-03-25 09:59:43 -0700

    Added have_writable_property matcher

--- spec/unit/spec.node.js
+++ spec/unit/spec.node.js
@@ -9,5 +9,15 @@ describe 'node'
         o.should.not.have_enumerable_property 'bar'
       end
     end
+    
+    describe 'have_writable_property'
+      it 'should check if a property is writable'
+        { foo: 'bar' }.should.have_writable_property 'foo'
+        var o = {}
+        Object.defineProperty(o, 'foo', { value: 'bar', writable: false })
+        o.should.not.have_writable_property 'foo'
+      end
+    end
+    
   end
 end
